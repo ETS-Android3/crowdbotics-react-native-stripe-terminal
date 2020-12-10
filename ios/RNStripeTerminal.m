6773d64dab3e4376e0a68498d27041bee5012921
Mohamed Hasan <designsuniverse@gmail.com>
2020-12-10 20:28:04 +0200

    Update RNStripeTerminal.m

--- ios/RNStripeTerminal.m
+++ ios/RNStripeTerminal.m
@@ -61,6 +61,7 @@ static dispatch_once_t onceToken = 0;
 {
     return @{
              @"DeviceTypeChipper2X": @(SCPDeviceTypeChipper2X),
+             @"DeviceTypeWisePad3": @(SCPDeviceTypeWisePad3),
 
              @"DiscoveryMethodBluetoothScan": @(SCPDiscoveryMethodBluetoothScan),
              @"DiscoveryMethodBluetoothProximity": @(SCPDiscoveryMethodBluetoothProximity),
